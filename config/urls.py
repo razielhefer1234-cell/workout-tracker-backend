9b3622c3da23eb0f06fe0e4a99c2eda5a062ba03
zz <razielhefer1234@gmail.com>
2026-09-18 18:31:07 +0300

    updated main urls.py

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('exercise/', include('exercises.urls')),
-    path('workout/', include('workout.urls')),
+    path('workout/', include('workouts.urls')),
 ]
