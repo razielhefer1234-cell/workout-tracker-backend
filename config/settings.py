73dd6a439b49b5769f8e8621c7dca6c2965d86c6
zz <razielhefer1234@gmail.com>
2026-09-18 18:28:22 +0300

    updated settings and added urls.py files for the apps

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'exercises',
+    'workouts',
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('exercise/', include('exercise.urls')),
+    path('exercise/', include('exercises.urls')),
     path('workout/', include('workout.urls')),
 ]
